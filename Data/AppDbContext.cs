8960cbf5dc0bb61de9adb2b40cf66c898f7d1671
Ayush <ayushwagh2002@gmail.com>
2025-06-20 15:23:42 +0530

    New commit

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace WebApplication1.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }  // I will be creating this
     }
 }
