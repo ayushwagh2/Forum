66dbfd9c501c6ee18ac02b3be13764a98b277b81
Ayush <ayushwagh2002@gmail.com>
2025-06-20 15:10:44 +0530

    Inital commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int Id { get; set; } // Primary Key
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
